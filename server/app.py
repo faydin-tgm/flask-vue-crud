8ac8f4990191db9b6b10ca7994d0550ae6b6d7c9
faydin-tgm <faydin@student.tgm.ac.at>
2019-01-11 13:24:52 +0100

    Port geändert, TODO-Liste und CRUD Befehle

--- server/app.py
+++ server/app.py
@@ -16,6 +16,16 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app)
 
+TODO = [
+    {
+        "id": uuid.uuid4().hex,
+        "todo": "test_Todo",
+        "assignee": "Test_assignee",
+        "done": False
+    }
+
+]
+
 BOOKS = [
     {
         'id': uuid.uuid4().hex,
@@ -39,6 +49,55 @@ BOOKS = [
         'price': '3.99'
     }
 ]
+
+
+
+@app.route('/todos', methods=['GET', 'POST'])
+def all_todos():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        TODO.append({
+            'id': uuid.uuid4().hex,
+            'todo': post_data.get('todo'),
+            'assignee': post_data.get('assignee'),
+            'done': post_data.get('done')
+        })
+        response_object['message'] = 'Todo added!'
+    else:
+        response_object['todo'] = TODO
+    return jsonify(response_object)
+
+
+@app.route('/todos/<todo_id>', methods=['GET', 'PUT', 'DELETE'])
+def single_todo(todo_id):
+    response_object = {'status': 'success'}
+    if request.method == 'GET':
+        # TODO: refactor to a lambda and filter
+        return_todo = ''
+        for todo in TODO:
+            if todo['id'] == todo_id:
+                return_todo = todo
+        response_object['todo'] = return_todo
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(todo_id)
+        TODO.append({
+            'id': uuid.uuid4().hex,
+            'todo': post_data.get('todo'),
+            'assignee': post_data.get('assignee'),
+            'done': post_data.get('done')
+        })
+        response_object['message'] = 'Todo updated!'
+    if request.method == 'DELETE':
+        remove_book(todo_id)
+        response_object['message'] = 'Todo removed!'
+    return jsonify(response_object)
+
+
+
+
+
 
 
 # sanity check route
@@ -128,5 +187,14 @@ def remove_book(book_id):
     return False
 
 
+def remove_todo(todo_id):
+    for todo in TODO:
+        if todo['id'] == todo_id:
+            TODO.remove(todo)
+            return True
+    return False
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8080)
