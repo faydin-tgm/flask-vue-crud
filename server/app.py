f074a8cac17bec8fd18f95ff1c22f84e7125137a
faydin-tgm <faydin@student.tgm.ac.at>
2019-01-18 15:17:02 +0100

    App.py: unnötigen Code entfernt

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 import os
 import uuid
 
-import stripe
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -68,46 +67,6 @@ def single_todo(todo_id):
         remove_todo(todo_id)
         response_object['message'] = 'Todo removed!'
     return jsonify(response_object)
-
-
-
-
-
-
-
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
-
-
-
-@app.route('/charge', methods=['POST'])
-def create_charge():
-    post_data = request.get_json()
-    amount = round(float(post_data.get('todo')['price']) * 100)
-    stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
-    charge = stripe.Charge.create(
-        amount=amount,
-        currency='usd',
-        card=post_data.get('token'),
-        description=post_data.get('todo')['title']
-    )
-    response_object = {
-        'status': 'success',
-        'charge': charge
-    }
-    return jsonify(response_object), 200
-
-
-@app.route('/charge/<charge_id>')
-def get_charge(charge_id):
-    stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
-    response_object = {
-        'status': 'success',
-        'charge': stripe.Charge.retrieve(charge_id)
-    }
-    return jsonify(response_object), 200
 
 
 def remove_todo(todo_id):
